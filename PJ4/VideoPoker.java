debd1f7549469eee0a9ebad0b50f0adff23f689e
Brian Lai <brianlai30@outlook.com>
2017-08-05 14:53:34 -0700

    commented out old code in sortHand. added new code to sortHand for sorting Rank

--- PJ4/VideoPoker.java
+++ PJ4/VideoPoker.java
@@ -234,26 +234,37 @@ public class VideoPoker {
     }
     
     private void sortHand() {
-        Card tempCardRank = null;
-        Card tempCardSuit = null;
-        for (int i = 0; i < this.playerHand.size(); i++) {
-            for (int j = i; j < this.playerHand.size(); j++) {
-                if (j + 1 < this.playerHand.size()) {
-                    if (this.playerHand.get(j).getRank() > this.playerHand.get(j+1).getRank()) {
-                        tempCardRank = this.playerHand.get(j);
-                        this.playerHand.add(j, this.playerHand.remove(j+1));
-                        this.playerHand.add(j+1, tempCardRank);
-                        tempCardRank = null;
-                    }
-                    if (this.playerHand.get(j).getSuit() > this.playerHand.get(j+1).getSuit()) {
-                        tempCardSuit = this.playerHand.get(j);
-                        this.playerHand.add(j, this.playerHand.remove(j+1));
-                        this.playerHand.add(j+1, tempCardSuit);
-                        tempCardSuit = null;
-                    }
+//        Card tempCardRank = null;
+//        Card tempCardSuit = null;
+//        for (int i = 0; i < this.playerHand.size(); i++) {
+//            for (int j = i; j < this.playerHand.size(); j++) {
+//                if (j + 1 < this.playerHand.size()) {
+//                    if (this.playerHand.get(j).getRank() > this.playerHand.get(j+1).getRank()) {
+//                        tempCardRank = this.playerHand.get(j);
+//                        this.playerHand.add(j, this.playerHand.remove(j+1));
+//                        this.playerHand.add(j+1, tempCardRank);
+//                        tempCardRank = null;
+//                    }
+//                    if (this.playerHand.get(j).getSuit() > this.playerHand.get(j+1).getSuit()) {
+//                        tempCardSuit = this.playerHand.get(j);
+//                        this.playerHand.add(j, this.playerHand.remove(j+1));
+//                        this.playerHand.add(j+1, tempCardSuit);
+//                        tempCardSuit = null;
+//                    }
+//                }
+//            }
+//        }
+        List<Card> sortRank = new ArrayList<Card>();
+        List<Card> sortSuit = new ArrayList<Card>();
+        for (int i = 0; i <= 13; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (this.playerHand.get(j).getRank() == i) {
+                    sortRank.add(this.playerHand.get(j));
                 }
             }
         }
+        this.playerHand.clear();
+        this.playerHand = sortRank;
     }
     
     
